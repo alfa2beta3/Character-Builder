3a355a10ee8f0cd756a0e3aa46fddb4ad3029037
alfa2beta3 <leexingyang180@gmail.com>
2023-08-13 22:20:32 +0800

    git commit -m "move contribute.dart"

---
+++ lib/contribute.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Contribute extends StatefulWidget {
+  const Contribute({Key? key}) : super(key: key);
+
+  @override
+  State<Contribute> createState() => _ContributeState();
+}
+
+class _ContributeState extends State<Contribute> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
